77821f7d54dde1ffe4618bae42bdab838516cfb0
jamiemcg10 <jamie.smart.10@gmail.com>
2024-07-29 00:44:01 -0400

    update svelte and sveltekit

---
+++ src/routes/api/+server.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import { json } from '@sveltejs/kit'
+
+export async function GET({ url }) {
+
+  const { searchParams } = url
+  const zipcode = searchParams.get('zipcode') 
+  
+  return json({
+    weather: await sendWeatherRequest(zipcode)}, 
+    { status: 201})
+}
+
+
+
+async function sendWeatherRequest(zipcode: string) {
+    const req = await fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zipcode},us&units=imperial&appid=${process.env.APIKEY}`)
+    
+    if (!req.ok){
+        return null
+    }
+
+    if (req.ok){
+        const json = req.json()
+        return json
+    }
+}
